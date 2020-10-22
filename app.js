d988d2768005b04705437e03dcacf3b62aea504f
Akash Nadadur <nsakashu@gmail.com>
2020-10-22 19:46:03 +0530

    node js routing

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter meassage</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text"><button type="submit">submit</button></form></body>'
+    );
+    res.write("</html>");
+
+    return res.end();
+  }
+//   if (url === "/message" && method === "POST") {
+//     fs.watchFileSync("message.txt", "DUMMY");
+//     res.statusCode = 302;
+//     res.setHeader("Location", "/");
+//     return res.end();
+//   }
+});
+
+server.listen(3000);
