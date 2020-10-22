c81cbdfc181a9ce88f00f70b8aec65dd719b58de
Akash Nadadur <nsakashu@gmail.com>
2020-10-22 20:31:57 +0530

    req.on() prb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+  //   const {method, headers, url} = req;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter meassage</title></head>");
@@ -14,12 +15,23 @@ const server = http.createServer((req, res) => {
 
     return res.end();
   }
-//   if (url === "/message" && method === "POST") {
-//     fs.watchFileSync("message.txt", "DUMMY");
-//     res.statusCode = 302;
-//     res.setHeader("Location", "/");
-//     return res.end();
-//   }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    console.log(body);
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
 });
 
 server.listen(3000);
